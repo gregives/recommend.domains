7f8f90a183563a8190ac31538be5e84cb8b9ae37
Greg Ives <greg@gregives.co.uk>
2023-04-19 19:25:07 +0100

    Create page specifically targeting Shopify

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Main } from "@/components/Main";
 export default function Home() {
   return (
     <main>
-      <Main />
+      <Main version="normal" />
       <Features />
       <Blog />
       <Footer />
