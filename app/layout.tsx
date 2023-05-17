1c4f851f54cb865163bf71660aa16646a185547d
Greg Ives <greg@gregives.co.uk>
2023-05-17 12:38:40 +0100

    Boost saturation of background

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,7 +131,7 @@ export default function RootLayout({
             </a>
           </nav>
         </header>
-        <div className="absolute -z-10 inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80 saturate-200 brightness-105">
+        <div className="absolute -z-10 inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80 saturate-[3] brightness-105">
           <svg
             className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
             viewBox="0 0 1155 678"
